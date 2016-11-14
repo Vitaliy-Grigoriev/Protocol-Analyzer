68dadf74cde7468229a786fe6f7d64e9c19416de
Vitaliy_Grigoriev <Vit.link420@gmail.com>
2016-11-14 05:03:22 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir build && cd build && cmake -j 4 .. && make -j 4
+mkdir build && cd build && cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -j4 .. && make -j4
 cd .. && rm -rf build
