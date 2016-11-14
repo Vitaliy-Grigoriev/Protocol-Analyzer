fcb6ce5f7eb92167b05b20330cce7507ecf953ba
Vitaliy_Grigoriev <Vit.link420@gmail.com>
2016-11-14 05:12:50 +0300

    Update cppcheck_scan.sh

--- cppcheck_scan.sh
+++ cppcheck_scan.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir check && cd check
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -j 4 ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -j4 ..
 cppcheck -q --language=c++ --template=vs -I/usr/include/ -I/usr/include/c++/6/ -I../include/ --std=c++11 --enable=all --project=compile_commands.json
 cd .. && rm -rf check
