de72f60aad2f43540e66b4b101581803ced33b96
Vitaliy-Grigoriev <Vit.link420@gmail.com>
2017-11-16 22:08:04 +0300

    PVS-Studio tuning.

--- pvs_scan.sh
+++ pvs_scan.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 mkdir check && cd check
+
+export COMPILER="/usr/bin/clang++"
+export BUILD_TYPE="Debug"
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On -j4 ..
-pvs-studio-analyzer analyze -o $(pwd)/tmp.plog -e /usr/include/ -j4
-plog-converter -a "GA;64;OP;CS" -t errorfile $(pwd)/tmp.plog
+
+pvs-studio-analyzer analyze -a "29" -r "/" --compiler clang++ -o $(pwd)/tmp.plog -e /usr/include/ -j4
+plog-converter -a "GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3" -d "V112,V122" --renderTypes=html -o "../pvs-result" $(pwd)/tmp.plog
+
 cd .. && rm -rf check
