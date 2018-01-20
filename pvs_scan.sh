fc7a73f39f354a8a07e936222ddcb6dae0edea55
Vitaliy-Grigoriev <Vit.link420@gmail.com>
2018-01-20 22:17:28 +0300

    [BUILD] Change Travis CI and PVS-Studio config files.

--- pvs_scan.sh
+++ pvs_scan.sh
@@ -7,6 +7,6 @@ export BUILD_TYPE="Debug"
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On -j4 ..
 
 pvs-studio-analyzer analyze -a "29" -r "/" --compiler clang++ -o $(pwd)/tmp.plog -e /usr/include/ -j4
-plog-converter -a "GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3" -d "V112,V122" --renderTypes=fullhtml -o "../" $(pwd)/tmp.plog
+plog-converter -a "GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3" -d "V112,V122" -m cwe --renderTypes=fullhtml -o "../" $(pwd)/tmp.plog
 
 cd .. && rm -rf check
