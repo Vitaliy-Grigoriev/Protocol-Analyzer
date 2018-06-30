9d3c202d6f3ecd227e65f4b22def2f50c2de68c8
Vitaliy-Grigoriev <Vit.link420@gmail.com>
2018-06-30 22:40:25 +0300

    [LIB/BUILD] Add Infer static scanner and fix build dependencies.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export COMPILER="/usr/bin/clang++"
+#export COMPILER="/usr/bin/g++"
+#export COMPILER="/opt/intel/bin/icc"
 export BUILD_TYPE="Debug"
 
 mkdir build && cd build && cmake -j 4 .. && make -j 4
---
+++ infer_scan.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export COMPILER="/usr/bin/clang++"
+export BUILD_TYPE="Debug"
+
+
+mkdir check && cd check
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -j4 ..
+infer run --compilation-database compile_commands.json
+
+cd .. && rm -rf check
