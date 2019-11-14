55abac5f6fa34258ca3d0a669aed0b2136b46cb4
Vitaliy_Grigoriev <Vitaliy-Grigoriev@users.noreply.github.com>
2019-11-15 02:50:20 +0300

    Update cppcheck_scan.sh

--- cppcheck_scan.sh
+++ cppcheck_scan.sh
@@ -6,6 +6,6 @@ export CXX="/usr/bin/clang++"
 export BUILD_TYPE="Debug"
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -j4 ..
 
-cppcheck -q --template=vs -I/usr/include/ -I/usr/include/c++/7.2.0 -I../include/framework/ --std=c++14 --enable=all --project=compile_commands.json
+cppcheck -q --template=vs -I/usr/include/ -I/usr/include/c++/7.2.0 -I../include/framework/ --std=c++17 --enable=all --project=compile_commands.json
 
 cd .. && rm -rf check
